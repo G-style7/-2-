9c59eede7c5abf1332e9f966165166eac4710de2
EC2 Default User <ec2-user@ip-172-31-3-171.ap-northeast-1.compute.internal>
2022-08-13 14:33:18 +0000

    check sheet19~

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,38 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all #Bookはモデル名　bookの全ての値を持ってくる
+    @book = Book.new
+  end
+
+  def create
+     @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id) #⬅️showページアクション
+    else
+      render :index
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    book.destroy # データ（レコード）を削除
+    redirect_to '/books' # 投稿一覧画面へリダイレクト
   end
 
   def update
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body) #bookはモデルがtitle.bodyカラムを承認
   end
 
 end
