e26e153de707d6bffc0286016ca361aaf34822e4
EC2 Default User <ec2-user@ip-172-31-3-171.ap-northeast-1.compute.internal>
2022-08-14 14:00:18 +0000

    ~25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,15 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all #Bookはモデル名　bookの全ての値を持ってくる
-    @book = Book.new
+   @books = Book.all #Bookはモデル名　bookの全ての値を持ってくる
+   @book = Book.new
   end
-
   def create
      @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id) #⬅️showページアクション
     else
+
       render :index
     end
   end
@@ -22,12 +23,17 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
+    book = Book.find(params[:id])
     book.destroy # データ（レコード）を削除
-    redirect_to '/books' # 投稿一覧画面へリダイレクト
+    redirect_to books_path # 投稿一覧画面へリダイレクト
+
   end
 
   def update
+   flash[:notice] = "Book was successfully updated."
+   book = Book.find(params[:id])
+   book.update(book_params)
+    redirect_to book_path(book.id)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :books, only: [:show, :index, :edit, :create, :destroy, :update] #コントローラー名と同じにする
-
+ #↑booksはコントローラー名と一緒
+ 
   # get 'books/index'
   # get 'books/show'
   # get 'books/edit'
