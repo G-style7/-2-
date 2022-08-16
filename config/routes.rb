e0580aef0623db16ce4493de9659caadcf87f783
EC2 Default User <ec2-user@ip-172-31-3-171.ap-northeast-1.compute.internal>
2022-08-16 04:58:10 +0000

    fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
   resources :books, only: [:show, :index, :edit, :create, :destroy, :update] #コントローラー名と同じにする
  #↑booksはコントローラー名と一緒
  
@@ -6,5 +7,4 @@ Rails.application.routes.draw do
   # get 'books/show'
   # get 'books/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root to: 'homes#top'
 end
